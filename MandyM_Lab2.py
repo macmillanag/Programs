24532bacab4c55b2d9155e4ca14b96bbe3c50fdf
Mandy MacMillan <mandymacmillan.eet@gmail.com>
2021-01-18 10:00:19 -0400

    Add files via upload

---
+++ MandyM_Lab2.py
@@ -0,0 +1,23 @@
+""" /**************************************************
+This is the first part of the training course of Python
+Instructor: Ricardo Bautista Quintero
+Date: 01/18/21
+NSCC Ivany Campus
+/**************************************************"""
+"""
+We will learn about the following topics:
+1.) Types of Numbers in Python
+2.) Basic Arithmetic
+3.) Differences between 
+classic division and floor division
+4.) Object Assignment in Python
+"""
+def program_1():
+a = 2
+f = 2e4
+print(a)
+print("is an integer")
+print(f)
+print("is an floating point number")
+if __name__ == '__main__':
+program_1()
